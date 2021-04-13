28b5cb2376d444da67a38922980907994b04290f
Joseph Kinnear <kinnearjc@gmail.com>
2021-04-13 14:06:32 -0400

    Added Ominous Message

--- script.js
+++ script.js
@@ -78,3 +78,47 @@ switch (badLuckColor) {
 
 console.log(`This month you should try in earnest to avoid the color ${avoidThis}.`);
 
+//ominous message
+
+let scaryMessageNum=(Math.floor(Math.random()*10));
+let ominousMessage="";
+switch (scaryMessageNum) {
+    case 0:
+        ominousMessage="The danger is still present, in your time, as it was in ours.";
+        break;
+    case 1:
+        ominousMessage="Any noise may act as an attraction. Be careful.";
+        break;
+    case 2:
+        ominousMessage="Running won't work. Fighting won't work. Begging won't work. Hiding may.";
+        break;
+    case 3:
+        ominousMessage="Never trust familiar voices.";
+        break;
+    case 4:
+        ominousMessage="Beware of decoys. Don't get yourself lured out.";
+        break;
+    case 5:
+        ominousMessage="You've seen it. Now he can hear you. You've touched it. Now he can see you. Never ring it. If you hear it, he can touch you.";
+        break;
+    case 6:
+        ominousMessage="Barricades are an attraction as they point out where you hide.";
+        break;
+    case 7:
+        ominousMessage="Do Not Enter. Death will follow.";
+        break;
+    case 8:
+        ominousMessage="Hush now! The subject spies on us!";
+        break;
+    case 9:
+        ominousMessage="Those who touch your food can reach inside your body.";
+        break;
+
+    default:
+        ominousMessage="Invalid ominous message"
+        break;
+}   
+
+console.log(`The spirits have gathered to share this important message with you:`);
+console.log(ominousMessage);
+
